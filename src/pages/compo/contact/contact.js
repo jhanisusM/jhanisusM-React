28553d69022f211289df2cf19133f073331ff899
jhanisusM <jhanisus@gmail.com>
2018-08-22 18:22:13 -0700

    added the form page. Will continue to add the functionality on next push

--- src/pages/compo/contact/contact.js
+++ src/pages/compo/contact/contact.js
@@ -1,51 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
 import Row from "../../../components/Row";
 import Hero from "../../../components/Hero";
 import Col from "../../../components/Col";
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
 import Container from "../../../components/Container";
 
-const contact = () => {
+class contact extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            email: "",
+            message: "",
+        };
+        this.handleInputChange = this.handleInputChange.bind(this)
+    };
 
-    
-    return (
-        <div>
-            <Hero backgroundImage={require("../../../images/laC.jpg")}>
-                <h3>Contact Me</h3>
-            </Hero>
-            <Container style={{ marginTop: 30 }}>
 
-                <Row>
-                    <Col size="md-12" id="title">
-                        <h4>Find me Here</h4>
-                        <hr />
-                        <p style={{ textAlign: "center" }} >
-                            <em>I'd love to hear from you </em>
-                        </p>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size="md-12">
-                        <div className="row">
-                            <div className="col-4" id="map">
+
+
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Hero backgroundImage={require("../../../images/laC.jpg")}>
+                    <h3>Contact Me</h3>
+                </Hero>
+                <Container style={{ marginTop: 30 }}>
+
+                    <Row>
+                        <Col size="md-12" id="title">
+                            <h4>Find me Here</h4>
+                            <hr />
+                            <p style={{ textAlign: "center" }} >
+                                <em>I'd love to hear from you </em>
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col size="md-12">
+                            <div className="row">
+                                <div className="col-4" id="map">
+                                </div>
+                                <div className="col-6">
+                                    <br />
+                                    City of Monrovia, CA, United States
+                        <br />
+                                    Phone: 213.948.6356
+                        <br />
+                                    Email: jhanisus@gmail.com
+                        <br />
+                                    <p>leave me a message:</p>
+
+                                </div>
                             </div>
-                            <div className="col-6">
-                                <br />
-                                 City of Monrovia, CA, United States
-                        <br />
-                              Phone: 213.948.6356
-                        <br />
-                              Email: jhanisus@gmail.com
-                        <br />
-                                <p>leave me a message:</p>
-                            
-                            </div>
-                        </div>
-
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+                            <Form style={{ width: '600px' }}>
+                                <FormGroup>
+                                    <Label for="name">Name:</Label>
+                                    <Input
+                                        type="text"
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Name (required)" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="email">Email:</Label>
+                                    <Input
+                                        type="email"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Eamil (required)" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="message">Message:</Label>
+                                    <Input
+                                        type="textarea"
+                                        name="message"
+                                        value={this.state.message}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Message"
+                                        style={{ height: 100 }} />
+                                </FormGroup>
+                                <Button>Send</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
 };
 
 export default contact;
