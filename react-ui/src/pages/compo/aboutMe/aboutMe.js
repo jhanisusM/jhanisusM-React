fb3a6eb872778ff6f46dafe5f153add99df0ad1d
jhanisusM <jhanisus@gmail.com>
2018-09-20 19:30:19 -0700

    Fixed cdd's css in about

--- react-ui/src/pages/compo/aboutMe/aboutMe.js
+++ react-ui/src/pages/compo/aboutMe/aboutMe.js
@@ -7,55 +7,55 @@ import "./aboutMe.css"
 
 
 const aboutMe = () => {
- return (
+    return (
 
-     <div>
-         <Hero backgroundImage={require("../../../images/la23.jpg")}>
-        <h1>Jhanisus Melendez</h1>
-        <a href="https://github.com/jhanisusM" className="btn btn-dark" target="blank">
-          <span className="fa fa-github"></span>
-        </a>
-        <a href="https://www.linkedin.com/in/jhanisus/" className="btn btn-dark" target="blank">
-          <span className="fa fa-linkedin-square"></span>
-        </a>
-        <a href="https://stackoverflow.com/users/8730147/jhanisusm" className="btn btn-dark" target="blank">
-          <span className="fa fa-stack-overflow"></span>
-        </a>
-        <br />
-        <br />
-        <span className="cdd">
-        <strong> <u> Code </u> </strong>
-        </span>
-        <span className="cdd">
-        <strong> <u>  Driven </u></strong>
-        </span>
-        <span className="cdd">
-        <strong><u> Design </u> </strong>
-        </span>
-      </Hero>
-         <Container style={{ marginTop: 30 }}>
-        <Row>
-            <Col size="md-12" id="title">
-                <h3>About Me</h3>
-                <hr />
-            </Col>
-        </Row>
-        <Row>
-            <Col size="md-12">
-                <div className="pic">
-                    <img src={require("../../../images/pic1.jpg")} alt="" />
+        <div>
+            <Hero backgroundImage={require("../../../images/la23.jpg")}>
+                <h1>Jhanisus Melendez</h1>
+                <a href="https://github.com/jhanisusM" className="btn btn-dark" target="blank">
+                    <span className="fa fa-github"></span>
+                </a>
+                <a href="https://www.linkedin.com/in/jhanisus/" className="btn btn-dark" target="blank">
+                    <span className="fa fa-linkedin-square"></span>
+                </a>
+                <a href="https://stackoverflow.com/users/8730147/jhanisusm" className="btn btn-dark" target="blank">
+                    <span className="fa fa-stack-overflow"></span>
+                </a>
+                <div className="cdd">
+                    <span className="cdd">
+                        <strong> <u> Code </u> </strong>
+                    </span>
+                    <span className="cdd">
+                        <strong> <u>  Driven </u></strong>
+                    </span>
+                    <span className="cdd">
+                        <strong><u> Design </u> </strong>
+                    </span>
                 </div>
-                <p>
-                    I am an analytical full stack web developer with 3 years as an image reading analyst. My attention to detail and adaptability
-                      to shifting priorities has afforded me the ability to facilitate and act as a liaison for various projects.
-                      I have a technical background in the hard sciences, and I am a UCLA bootcamp alumnus with experience in dynamic
-                      web applications using Mongodb, MySQL, Express, Node JS, React, HTML/CSS, and C++. Driven to exceed customer
-                      expectations by delivering valuable well-designed products that innovate technical solutions.
+            </Hero>
+            <Container style={{ marginTop: 30 }}>
+                <Row>
+                    <Col size="md-12" id="title">
+                        <h3>About Me</h3>
+                        <hr />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <div className="pic">
+                            <img src={require("../../../images/pic1.jpg")} alt="" />
+                        </div>
+                        <p>
+                            I am an analytical full stack web developer with 3 years as an image reading analyst. My attention to detail and adaptability
+                              to shifting priorities has afforded me the ability to facilitate and act as a liaison for various projects.
+                              I have a technical background in the hard sciences, and I am a UCLA bootcamp alumnus with experience in dynamic
+                              web applications using Mongodb, MySQL, Express, Node JS, React, HTML/CSS, and C++. Driven to exceed customer
+                              expectations by delivering valuable well-designed products that innovate technical solutions.
           </p>
-            </Col>
-        </Row>
-         </Container>
-    </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 };
 
